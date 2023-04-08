1387f66ffc44595612eade8694464c6b6370974e
Rafael <rafael.costa.ti@gmail.com>
2023-04-08 19:51:27 -0300

    updating Colors

--- app/src/main/java/com/orafaelsc/composeplayground/ui/theme/Color.kt
+++ app/src/main/java/com/orafaelsc/composeplayground/ui/theme/Color.kt
@@ -1,8 +1,68 @@
 package com.orafaelsc.composeplayground.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.orafaelsc.composeplayground.R
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val lightColors: Colors
+    @Composable
+    get() = Colors(
+        //primary
+        primary = colorResource(id = R.color.light_primary),
+        primaryVariant = colorResource(id = R.color.light_primary_light),
+        onPrimary = colorResource(id = R.color.light_on_primary),
+        secondary = colorResource(id = R.color.light_secondary),
+        secondaryVariant = colorResource(id = R.color.light_secondary_light),
+        onSecondary = colorResource(id = R.color.light_on_secondary),
+        background = colorResource(id = R.color.light_background),
+        onBackground = colorResource(id = R.color.light_primary_text),
+        surface = colorResource(id = R.color.light_background),
+        error = colorResource(id = R.color.light_error),
+        onError = colorResource(id = R.color.light_on_error),
+        onSurface = colorResource(id = R.color.light_surface),
+        isLight = true
+    )
+
+val darkColors: Colors
+    @Composable
+    get() = Colors(
+        primary = colorResource(id = R.color.dark_primary),
+        primaryVariant = colorResource(id = R.color.dark_primary_light),
+        onPrimary = colorResource(id = R.color.dark_on_primary),
+        secondary = colorResource(id = R.color.dark_secondary),
+        secondaryVariant = colorResource(id = R.color.dark_secondary_light),
+        onSecondary = colorResource(id = R.color.dark_on_secondary),
+        background = colorResource(id = R.color.dark_background),
+        onBackground = colorResource(id = R.color.dark_primary_text),
+        surface = colorResource(id = R.color.dark_background),
+        error = colorResource(id = R.color.dark_error),
+        onError = colorResource(id = R.color.dark_on_error),
+        onSurface = colorResource(id = R.color.dark_surface),
+        isLight = false
+    )
+
+@Immutable
+data class ExtendedColors(
+    val companyAlert: Color
+)
+
+val localExtendedColors = staticCompositionLocalOf {
+    ExtendedColors(
+        companyAlert = Color.Unspecified
+    )
+}
+
+val lightExtendedColors: ExtendedColors
+    @Composable
+    get() = ExtendedColors(
+        companyAlert = colorResource(id = R.color.light_company_alert)
+    )
+val darkExtendedColors: ExtendedColors
+    @Composable
+    get() = ExtendedColors(
+        companyAlert = colorResource(id = R.color.dark_company_alert)
+    )
