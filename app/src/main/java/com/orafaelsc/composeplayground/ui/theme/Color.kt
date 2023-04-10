368e271bae3cd6656f6627029d4024138f5670a4
Rafael <rafael.costa.ti@gmail.com>
2023-04-10 20:39:37 -0300

    Update colors, Theme.kt, Shape.kt, Type.kt and Color.kt to use material 3

--- app/src/main/java/com/orafaelsc/composeplayground/ui/theme/Color.kt
+++ app/src/main/java/com/orafaelsc/composeplayground/ui/theme/Color.kt
@@ -1,128 +1,104 @@
 package com.orafaelsc.composeplayground.ui.theme
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Colors
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import com.orafaelsc.composeplayground.R
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.orafaelsc.composeplayground.R
 
-val lightColors: Colors
-    @Composable
-    get() = Colors(
-        //primary
-        primary = colorResource(id = R.color.light_primary),
-        primaryVariant = colorResource(id = R.color.light_primary_light),
-        onPrimary = colorResource(id = R.color.light_on_primary),
-        secondary = colorResource(id = R.color.light_secondary),
-        secondaryVariant = colorResource(id = R.color.light_secondary_light),
-        onSecondary = colorResource(id = R.color.light_on_secondary),
-        background = colorResource(id = R.color.light_background),
-        onBackground = colorResource(id = R.color.light_primary_text),
-        surface = colorResource(id = R.color.light_background),
-        error = colorResource(id = R.color.light_error),
-        onError = colorResource(id = R.color.light_on_error),
-        onSurface = colorResource(id = R.color.light_surface),
-        isLight = true
-    )
 
-val darkColors: Colors
-    @Composable
-    get() = Colors(
-        primary = colorResource(id = R.color.dark_primary),
-        primaryVariant = colorResource(id = R.color.dark_primary_light),
-        onPrimary = colorResource(id = R.color.dark_on_primary),
-        secondary = colorResource(id = R.color.dark_secondary),
-        secondaryVariant = colorResource(id = R.color.dark_secondary_light),
-        onSecondary = colorResource(id = R.color.dark_on_secondary),
-        background = colorResource(id = R.color.dark_background),
-        onBackground = colorResource(id = R.color.dark_primary_text),
-        surface = colorResource(id = R.color.dark_background),
-        error = colorResource(id = R.color.dark_error),
-        onError = colorResource(id = R.color.dark_on_error),
-        onSurface = colorResource(id = R.color.dark_surface),
-        isLight = false
-    )
-
-@Immutable
-data class ExtendedColors(
-    val companyAlert: Color
+val LightThemeColors: ColorScheme @Composable get() = lightColorScheme(
+    primary = colorResource(id = R.color.light_primary),
+    onPrimary = colorResource(id = R.color.light_onPrimary),
+    primaryContainer = colorResource(id = R.color.light_primaryContainer),
+    onPrimaryContainer = colorResource(id = R.color.light_onPrimaryContainer),
+    secondary = colorResource(id = R.color.light_secondary),
+    onSecondary = colorResource(id = R.color.light_onSecondary),
+    secondaryContainer = colorResource(id = R.color.light_secondaryContainer),
+    onSecondaryContainer = colorResource(id = R.color.light_onSecondaryContainer),
+    tertiary = colorResource(id = R.color.light_tertiary),
+    onTertiary = colorResource(id = R.color.light_onTertiary),
+    tertiaryContainer = colorResource(id = R.color.light_tertiaryContainer),
+    onTertiaryContainer = colorResource(id = R.color.light_onTertiaryContainer),
+    error = colorResource(id = R.color.light_error),
+    errorContainer = colorResource(id = R.color.light_errorContainer),
+    onError = colorResource(id = R.color.light_onError),
+    onErrorContainer = colorResource(id = R.color.light_onErrorContainer),
+    background = colorResource(id = R.color.light_background),
+    onBackground = colorResource(id = R.color.light_onBackground),
+    surface = colorResource(id = R.color.light_surface),
+    onSurface = colorResource(id = R.color.light_onSurface),
+    surfaceVariant = colorResource(id = R.color.light_surfaceVariant),
+    onSurfaceVariant = colorResource(id = R.color.light_onSurfaceVariant),
+    outline = colorResource(id = R.color.light_outline),
+    inverseOnSurface = colorResource(id = R.color.light_inverseOnSurface),
+    inverseSurface = colorResource(id = R.color.light_inverseSurface),
+    inversePrimary = colorResource(id = R.color.light_inversePrimary),
 )
 
-val localExtendedColors = staticCompositionLocalOf {
+val DarkThemeColors: ColorScheme @Composable get() = darkColorScheme(
+    primary = colorResource(id = R.color.dark_primary),
+    onPrimary = colorResource(id = R.color.dark_onPrimary),
+    primaryContainer = colorResource(id = R.color.dark_primaryContainer),
+    onPrimaryContainer = colorResource(id = R.color.dark_onPrimaryContainer),
+    secondary = colorResource(id = R.color.dark_secondary),
+    onSecondary = colorResource(id = R.color.dark_onSecondary),
+    secondaryContainer = colorResource(id = R.color.dark_secondaryContainer),
+    onSecondaryContainer = colorResource(id = R.color.dark_onSecondaryContainer),
+    tertiary = colorResource(id = R.color.dark_tertiary),
+    onTertiary = colorResource(id = R.color.dark_onTertiary),
+    tertiaryContainer = colorResource(id = R.color.dark_tertiaryContainer),
+    onTertiaryContainer = colorResource(id = R.color.dark_onTertiaryContainer),
+    error = colorResource(id = R.color.dark_error),
+    errorContainer = colorResource(id = R.color.dark_errorContainer),
+    onError = colorResource(id = R.color.dark_onError),
+    onErrorContainer = colorResource(id = R.color.dark_onErrorContainer),
+    background = colorResource(id = R.color.dark_background),
+    onBackground = colorResource(id = R.color.dark_onBackground),
+    surface = colorResource(id = R.color.dark_surface),
+    onSurface = colorResource(id = R.color.dark_onSurface),
+    surfaceVariant = colorResource(id = R.color.dark_surfaceVariant),
+    onSurfaceVariant = colorResource(id = R.color.dark_onSurfaceVariant),
+    outline = colorResource(id = R.color.dark_outline),
+    inverseOnSurface = colorResource(id = R.color.dark_inverseOnSurface),
+    inverseSurface = colorResource(id = R.color.dark_inverseSurface),
+    inversePrimary = colorResource(id = R.color.dark_inversePrimary),
+)
+
+
+/**
+ * Base class to extend the material color theme with auxiliary colors that goes
+ * beyond the default theme acc. to accomplish your company's corporate identity design
+ */
+@Immutable
+data class ExtendedColors(
+    val companyBlue: Color,
+    val companyRed: Color,
+)
+
+val LocalExtendedColors = staticCompositionLocalOf {
     ExtendedColors(
-        companyAlert = Color.Unspecified
+        companyBlue = Color.Unspecified,
+        companyRed = Color.Unspecified,
     )
 }
 
+// day colors extented
 val lightExtendedColors: ExtendedColors
     @Composable
     get() = ExtendedColors(
-        companyAlert = colorResource(id = R.color.light_company_alert)
+        companyBlue = colorResource(id = R.color.light_company_blue),
+        companyRed = colorResource(id = R.color.light_company_red),
     )
+
+// night colors extended
 val darkExtendedColors: ExtendedColors
     @Composable
     get() = ExtendedColors(
-        companyAlert = colorResource(id = R.color.dark_company_alert)
+        companyBlue = colorResource(id = R.color.dark_company_blue),
+        companyRed = colorResource(id = R.color.dark_company_red),
     )
-
-
-@Preview
-@Composable
-fun LightColorsPreview() {
-    ComposePlaygroundTheme(darkTheme = false) {
-        ColorList()
-    }
-}
-
-@Preview
-@Composable
-fun DarkColorsPreview() {
-    ComposePlaygroundTheme(darkTheme = true) {
-        ColorList()
-    }
-}
-
-@Composable
-fun ColorList() {
-    Column(
-        modifier = Modifier
-            .verticalScroll(
-                state = rememberScrollState()
-            )
-            .fillMaxHeight()
-    ) {
-        mapOf(
-            "primary" to MaterialTheme.colors.primary,
-            "primaryVariant" to MaterialTheme.colors.primaryVariant,
-            "onPrimary" to MaterialTheme.colors.onPrimary,
-            "secondary" to MaterialTheme.colors.secondary,
-            "secondaryVariant" to MaterialTheme.colors.secondaryVariant,
-            "onSecondary" to MaterialTheme.colors.onSecondary,
-            "background" to MaterialTheme.colors.background,
-            "onBackground" to MaterialTheme.colors.onBackground,
-            "surface" to MaterialTheme.colors.surface,
-            "error " to MaterialTheme.colors.error,
-            "onError" to MaterialTheme.colors.onError,
-            "onSurface" to MaterialTheme.colors.onSurface,
-        ).forEach { (text, color) ->
-            Row {
-                Text(text = text, modifier = Modifier
-                    .weight(2f)
-                    .background(color)
-                    .height(60.dp)
-                )
-            }
-        }
-    }
-}
--- app/src/main/java/com/orafaelsc/composeplayground/ui/theme/Theme.kt
+++ app/src/main/java/com/orafaelsc/composeplayground/ui/theme/Theme.kt
@@ -1,22 +1,49 @@
 package com.orafaelsc.composeplayground.ui.theme
 
+import android.os.Build
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun ComposePlaygroundTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    isDynamic: Boolean = true,
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val extendedColors = if (darkTheme) darkExtendedColors else lightExtendedColors
+    val systemUiController = rememberSystemUiController()
+    val useDynamicColor = isDynamic && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val extendedColors = if (useDarkTheme) darkExtendedColors else lightExtendedColors
 
-    CompositionLocalProvider(values = arrayOf(localExtendedColors provides extendedColors)) {
+    val colors = if (useDynamicColor) {
+        val context = LocalContext.current
+        if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    } else {
+        if (useDarkTheme) DarkThemeColors else LightThemeColors
+    }
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = colors.primary
+        )
+    }
+
+    CompositionLocalProvider(LocalExtendedColors provides extendedColors) {
         MaterialTheme(
-            colors = if (darkTheme) darkColors else lightColors,
+            colorScheme = colors,
             typography = AppDefaultTypography,
-            shapes = Shapes,
+            shapes = AppShapes,
             content = content
         )
     }
@@ -29,7 +56,88 @@ object ComposePlaygroundTheme {
         get() = AppCustomTypography
 
     val colorsCustom: ExtendedColors
-    @Composable
-    get() = localExtendedColors.current
+        @Composable
+        get() = LocalExtendedColors.current
 
+}
+
+
+@Preview
+@Composable
+fun LightColorsPreview() {
+    ComposePlaygroundTheme(useDarkTheme = false) {
+        ColorList()
+    }
+}
+
+@Preview
+@Composable
+fun DarkColorsPreview() {
+    ComposePlaygroundTheme(useDarkTheme = true) {
+        ColorList()
+    }
+}
+
+@Composable
+fun ColorList() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(state = rememberScrollState())
+            .fillMaxHeight()
+    ) {
+        mapOf(
+            "primary" to MaterialTheme.colorScheme.primary,
+            "primaryVariant" to MaterialTheme.colorScheme.primaryContainer,
+            "primarySurface" to MaterialTheme.colorScheme.onPrimaryContainer,
+            "onPrimary" to MaterialTheme.colorScheme.onPrimary,
+            "inversePrimary" to MaterialTheme.colorScheme.inversePrimary,
+
+            "secondary" to MaterialTheme.colorScheme.secondary,
+            "onSecondary" to MaterialTheme.colorScheme.onSecondary,
+            "secondaryVariant" to MaterialTheme.colorScheme.secondaryContainer,
+            "onSecondaryContainer" to MaterialTheme.colorScheme.onSecondaryContainer,
+
+            "tertiary" to MaterialTheme.colorScheme.tertiary,
+            "onTertiary" to MaterialTheme.colorScheme.onTertiary,
+            "tertiaryContainer" to MaterialTheme.colorScheme.tertiaryContainer,
+            "onTertiaryContainer" to MaterialTheme.colorScheme.onTertiaryContainer,
+
+            "background" to MaterialTheme.colorScheme.background,
+            "onBackground" to MaterialTheme.colorScheme.onBackground,
+
+            "surface" to MaterialTheme.colorScheme.surface,
+            "onSurface" to MaterialTheme.colorScheme.onSurface,
+            "surfaceVariant" to MaterialTheme.colorScheme.surfaceVariant,
+            "onSurfaceVariant" to MaterialTheme.colorScheme.onSurfaceVariant,
+            "surfaceTint" to MaterialTheme.colorScheme.surfaceTint,
+            "inverseSurface" to MaterialTheme.colorScheme.inverseSurface,
+            "inverseOnSurface" to MaterialTheme.colorScheme.inverseOnSurface,
+
+            "error" to MaterialTheme.colorScheme.error,
+            "onError" to MaterialTheme.colorScheme.onError,
+            "errorContainer" to MaterialTheme.colorScheme.errorContainer,
+            "onErrorContainer" to MaterialTheme.colorScheme.onErrorContainer,
+            "outline" to MaterialTheme.colorScheme.outline,
+
+            // custom extented colors
+            "companyBlue" to ComposePlaygroundTheme.colorsCustom.companyBlue,
+            "companyREd" to ComposePlaygroundTheme.colorsCustom.companyRed,
+
+            ).forEach { (text, color) ->
+            Row {
+                Text(
+                    text = text,
+                    color = ComposePlaygroundTheme.colorsCustom.companyBlue,
+                    modifier = Modifier
+                        .weight(1f)
+                )
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(32.dp)
+                        .background(color)
+                )
+            }
+        }
+    }
 }
